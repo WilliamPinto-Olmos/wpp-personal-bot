3c4314cd9b8a9e704f1c335abdc8a412ada1c1db
William P. <wpinto@sisu.mx>
2026-01-05 19:13:40 -0600

    build: minor changes to support custom firestore databases.

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,8 +1,18 @@
-import { initializeApp } from 'firebase-admin/app';
+import admin from 'firebase-admin';
+import serviceAccount from './firebase.json' with { type: 'json' };
 import { getFirestore } from 'firebase-admin/firestore';
 
-const app = initializeApp();
+admin.initializeApp({
+  credential: admin.credential.cert((serviceAccount) as admin.ServiceAccount),
+  projectId: serviceAccount.project_id,
+});
 
-const db = getFirestore(app);
+const customDatabase = process.env.FIRESTORE_DATABASE_NAME;
 
-export { db };
+const db = customDatabase ? getFirestore(admin.app(), customDatabase) : getFirestore(admin.app());
+
+db.settings({
+    ignoreUndefinedProperties: true
+})
+
+export {db }
